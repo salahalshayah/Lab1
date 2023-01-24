3d37107dd0789cb38ca8290d4fa3beb7bdefb68d
Salah Al Shayah <122882654+salahalshayah@users.noreply.github.com>
2023-01-24 20:48:29 +0200

    Create Problems.java
    FIxed the errors

---
+++ Problems.java
@@ -0,0 +1,7 @@
+public class Problems {
+	public static void main(String[] args) {
+		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("Hello, there are a lot");
+		System.out.print("of problems");
+	}
+}
